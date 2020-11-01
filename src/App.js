d18e68d36621065b945a4ab09d589a6664854e2f
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-11-01 18:51:12 +0530

    Skills JS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import About from './components/About';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <About />
+      <Skills />
     </>
   );
 }
